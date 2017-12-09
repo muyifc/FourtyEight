56aafc658fd0961a1bf1bde9099e769caee59b35
linjinbo <821458278@qq.com>
2017-12-09 20:58:21 +0800

    no message

--- Unity/Play_Shadow/Assets/Scripts/Data.cs
+++ Unity/Play_Shadow/Assets/Scripts/Data.cs
@@ -19,28 +19,9 @@ public class Data : Singleton<Data>
     public string[] matchItemNameArray_1 = new string[] { "HuaBi", "ShaoZi" };
 
 
-
     public Vector3[] collectPosDirtyArray_2 = new Vector3[] { new Vector3(1f, 2f, 3f), new Vector3(12f, 2f, 1f) };
     public Vector3[] collectPosDirtyArray_3 = new Vector3[] { new Vector3(1f, 2f, 3f), new Vector3(12f, 2f, 1f) };
 
-
-
-
-<<<<<<< HEAD
-
-
-    //剧情
-    public string PlotDesc1 = "我看不到自己，却只有在篝火的映照下才能感受到自己微弱的存在。";
-    public string PlotDesc2 = "我还活着吗？或许吧。";
-    public string PlotDesc3 = "而我唯一可以确认的是，一旦篝火熄灭，那点微弱的存在也将消失。";
-
-
-    public string PlotIcon1 = "Image/plot1";
-    public string PlotIcon2 = "Image/2";
-    public string PlotIcon3 = "Image/3";
-
-
-=======
 	//剧情
 	public string PlotDesc1 = "我看不到自己，却只有在篝火的映照下才能感受到自己微弱的存在。";
 	public string PlotDesc2 = "我还活着吗？或许吧。";
@@ -53,5 +34,5 @@ public class Data : Singleton<Data>
 	public string PlotAudio1 = "Audio/plot1";
 	public string PlotAudio2 = "Audio/plot2";
 	public string PlotAudio3 = "Audio/plot3";
->>>>>>> 3f4be7fddc7e410b95fa45f6e470fa536bc89dd4
+
 }
