79bc92a0e718f8a81aed88b9408f2e8ce08597d1
zhangxiaowei <1556726976@qq.com>
2017-12-10 02:55:10 +0800

    no message

--- Unity/Play_Shadow/Assets/Scripts/Core/RolerController.cs
+++ Unity/Play_Shadow/Assets/Scripts/Core/RolerController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DragonBones;
 
 public class RolerController : MonoBehaviour
 {
     public static RolerController Instance;
     private iTweenEvent tweenEvent;
+    public UnityArmatureComponent anim;
     void Awake()
     {
         Instance = this;
+        this.anim.animation.Play("stand");
     }
 
     public void AutoMove(iTweenPath path)
@@ -40,11 +43,13 @@ public class RolerController : MonoBehaviour
     //开始移动
     void AnimationStart(float f)
     {
+        this.anim.animation.Play("walk");
         Debug.Log("start :" + f);
     }
     //移动结束
     void AnimationEnd(string f)
     {
+        this.anim.animation.Play("stand");
         Debug.Log("end : " + f);
         SceneManager.Instance.SwitchLevelPart();
     }
